dae50003e5ccc4ec68b6793f1005e34a87d477b3
wnstjrdl <wnstjr490@naver.com>
2022-06-28 15:33:20 +0900

    프로젝트시작

---
+++ src/main/java/com/its/member/MemberBoardProject20220628Application.java
@@ -0,0 +1,13 @@
+package com.its.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberBoardProject20220628Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberBoardProject20220628Application.class, args);
+    }
+
+}
